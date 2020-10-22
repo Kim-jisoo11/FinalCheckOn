ece82e1656ccdc37519d1b83ee5ec93eb2f9c762
김지수 <kingwltn@naver.com>
2020-10-23 07:09:54 +0900

    0개주문 오류 해결 / 미선택 delete 해결중

--- checkonproject/shop/views.py
+++ checkonproject/shop/views.py
@@ -35,16 +35,14 @@ def cart(request):
     posts = paginator.get_page(page)
     total_prices = 0
     for i in cart:
-        print(i)
         i.products.price = i.products.price * i.quantity
         total_prices = total_prices + i.products.price    
     cart.totalAmount = total_prices
-    print(cart.totalAmount)
+    
     
 
     # 카테고리별 산 상품 종류 합계
     isBought = {}
-    print(type(isBought))
     for i in cart:
         if i.category_id in isBought:
             sum = isBought.get(i.category_id) + 1
@@ -56,7 +54,7 @@ def cart(request):
     totalSum=0
     for key, value in isBought.items():
         totalSum = totalSum + value
-        print(key, " : ", value)
+    
 
     # 카테고리 통계
     countProduct = {}
@@ -64,9 +62,9 @@ def cart(request):
         countProduct[i.category_id] = isBought.get(i.category_id) / totalSum * 100
 
     for key, value in countProduct.items():
-        print(key, " : ", value)
+    
 
-    context = {'cart': cart, 'categories': categories, 'posts' : posts, 'countProduct' : countProduct, 'totalSum' : totalSum}
+        context = {'cart': cart, 'categories': categories, 'posts' : posts, 'countProduct' : countProduct, 'totalSum' : totalSum}
 
     return render(request, 'cart.html', context)
 
@@ -79,16 +77,15 @@ def mypage(request):
     posts = paginator.get_page(page)
     total_prices = 0
     for i in cart:
-        print(i)
         i.products.price = i.products.price * i.quantity
         total_prices = total_prices + i.products.price    
     cart.totalAmount = total_prices
-    print(cart.totalAmount)
+ 
     
 
     # 카테고리별 산 상품 종류 합계
     isBought = {}
-    print(type(isBought))
+
     for i in cart:
         if i.category_id in isBought:
             sum = isBought.get(i.category_id) + 1
@@ -100,7 +97,7 @@ def mypage(request):
     totalSum=0
     for key, value in isBought.items():
         totalSum = totalSum + value
-        print(key, " : ", value)
+        
 
     # 카테고리 통계
     countProduct = {}
@@ -115,27 +112,21 @@ def mypage(request):
         # productCategory(i.caegory_id) = "과일, 견과"
         # print_category = {"과일, 견과":14.28...}
 
-    for key, value in print_category.items():
-        print(key, " : ", value)
-        #출력 확인용
-    for key, value in countProduct.items():
-        print(key, " : ", value)
-        #출력 확인용
-    
+  
     
     
     
     # sorting-> value값 기준으로 내림차순 정렬
     sortProductTuple = sorted(countProduct.items(), key=operator.itemgetter(1), reverse=True)
     firstrank = sortProductTuple[0][0]
-    print(type(firstrank))
+   
     
     # 타이틀 분류 (switch문과 비슷)
     def f(x):
         return {1:"도토리보따리 다람쥐", 2:"연어킬러 곰곰이", 3:"단백질 백만장자 고릴라", 4:"채소 요정 코끼리", 5:"방앗간도둑 참새",
                 6:"얼룩얼룩 송아지", 7:"귀차니즘 나무늘보", 8:"수분부자 하마", 9:"살아있는 진저브레드 쿠키", 10:"휘리릭 냠냠 토끼", 11:"앞날창창 거북이"}[x]
     title = f(firstrank)
-    print(title)
+    
 
     context = {'cart': cart, 'categories': categories, 'posts' : posts, 'countProduct' : countProduct, 'title' : title, 'firstrank':firstrank, "productCategory":productCategory, "print_category":print_category}
 
